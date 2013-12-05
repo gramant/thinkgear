3dd1a2de679778a5967fdd5f0b9122561216d447
fedor.belov <fbelov@gramant.ru>
2013-12-05 16:34:32 +0400

    blink + sleep stage changes

--- src/ru/gramant/thinkgear/Params.java
+++ src/ru/gramant/thinkgear/Params.java
@@ -1,5 +1,9 @@
 package ru.gramant.thinkgear;
 
+import org.apache.commons.lang.ArrayUtils;
+
+import java.util.List;
+
 import ru.gramant.thinkgear.utils.FormatUtils;
 
 /**
@@ -17,14 +21,13 @@ public class Params {
     public int theta;
     public int attention;
     public int meditation;
-    public int blink;
+    public List<Integer> blink;
     public int rawCount;
     public int rawValue;
     public int poorSignal;
-    public int heartRate;
-    public int rrInt;
+    public int sleepStage;
 
-    public Params(int delta, int highAlpha, int highBeta, int lowAlpha, int lowBeta, int lowGamma, int midGamma, int theta, int attention, int meditation, int blink, int rawCount, int rawValue, int poorSignal, int heartRate, int rrInt) {
+    public Params(int delta, int highAlpha, int highBeta, int lowAlpha, int lowBeta, int lowGamma, int midGamma, int theta, int attention, int meditation, List<Integer> blink, int rawCount, int rawValue, int poorSignal, int sleepStage) {
         this.delta = delta;
         this.highAlpha = highAlpha;
         this.highBeta = highBeta;
@@ -39,16 +42,15 @@ public class Params {
         this.rawCount = rawCount;
         this.rawValue = rawValue;
         this.poorSignal = poorSignal;
-        this.heartRate = heartRate;
-        this.rrInt = rrInt;
+        this.sleepStage = sleepStage;
     }
 
     public static String[] getLogParamNames() {
-        return new String[]{"delta", "highAlpha", "highBeta", "lowAlpha", "lowBeta", "lowGamma", "midGamma", "theta", "attention", "meditation", "blink", "rawCount", "rawValue", "poorSignal", "heartRate", "rrInt"};
+        return new String[]{"delta", "highAlpha", "highBeta", "lowAlpha", "lowBeta", "lowGamma", "midGamma", "theta", "attention", "meditation", "rawCount", "rawValue", "poorSignal", "sleepStage", "blink"};
     }
 
     public Integer[] getLogParams() {
-        return new Integer[]{delta, highAlpha, highBeta, lowAlpha, lowBeta, lowGamma, midGamma, theta, attention, meditation, blink, rawCount, rawValue, poorSignal, heartRate, rrInt};
+        return (Integer[]) ArrayUtils.addAll(new Integer[]{delta, highAlpha, highBeta, lowAlpha, lowBeta, lowGamma, midGamma, theta, attention, meditation, rawCount, rawValue, poorSignal, sleepStage}, blink.toArray(new Integer[blink.size()]));
     }
 
     public static String[] getHistoryConfigNames() {
